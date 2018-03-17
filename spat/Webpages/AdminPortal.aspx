8af0f43dd1e9c2ec28a6a4d405e919d0a41b09a4
Mike Szucsik <mszucsik@hotmail.com>
2018-03-16 22:39:56 -0400

    admin portal ui added

--- spat/Webpages/AdminPortal.aspx
+++ spat/Webpages/AdminPortal.aspx
@@ -4,21 +4,68 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>SpAT LIGHT - Administration</title>
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="../../Content/admin.css" rel="stylesheet" />
 </head>
 <body>
-    <form id="adminPortal" runat="server">
-        <div runat="server" id="signIn" visible="true">
-            <asp:Label ID="lblUserName" runat="server" Text="User name: "></asp:Label><br />
-            <asp:TextBox ID="tbUserName" runat="server"></asp:TextBox><br />
-            <asp:Label ID="lblPassword" runat="server" Text="Password: "></asp:Label><br />
-            <asp:TextBox ID="tbPassword" runat="server" type="password" TabIndex="1"></asp:TextBox><br />
-            <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" TabIndex="2" /><br />
-            <asp:Label ID="lblError" runat="server" ForeColor="Red" Font-Bold="False"></asp:Label>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-expand-md navbar-light" id="mainNav">
+        <div class="container">
+            SpAT LIGHT Administration
+      <button class="navbar-toggler navbar-toggler-right no-border" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+          aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <i class="fa fa-bars"></i>
+      </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav navbar-right ml-auto">
+                    <li class="nav-item dropdown">Manage
+             <div class="dropdown-content">
+                 <a href="#">Questions</a>
+                 <a href="#">Resources</a>
+                 <a href="#">Password</a>
+             </div>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#">Statistics</a>
+                    </li>
+                    <li class="nav-item" hidden="yup">
+                        <a>Logout</a>
+                    </li>
+            </div>
         </div>
-        <div runat="server" id="adminPortalForm" visible="false">
-            <h1>Alter survey questions: </h1>
+    </nav>
+
+    <!-- Login Form -->
+    <div class="text-center">
+        <div class="container">
+            <div class="row">
+                <div class="login-form mx-auto">
+                    <div class="row">
+                        <form id="adminPortal" runat="server">
+                            <div runat="server" id="signIn" visible="true">
+                                <h5>Login</h5>
+                                <asp:Label class="lbl-input" ID="lblUserName" runat="server" Text="Username"></asp:Label><br />
+                                <asp:TextBox class="txt-input" ID="tbUserName" runat="server"></asp:TextBox><br />
+                                <asp:Label class="lbl-input" ID="lblPassword" runat="server" Text="Password"></asp:Label><br />
+                                <asp:TextBox class="txt-input" ID="tbPassword" runat="server" type="password" TabIndex="1"></asp:TextBox><br />
+                                <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" TabIndex="2" class="btn btn-basic btn-login" /><br />
+                                <asp:Label class="lbl-input" ID="lblError" runat="server" ForeColor="#FF4136" Font-Bold="False"></asp:Label>
+                            </div>
+                            <div runat="server" id="adminPortalForm" visible="false">
+                                <h2>Manage Survey Questions</h2>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-    </form>
+    </div>
+
+    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../Scripts/bootstrap.min.js"></script>
+    <script src="../Scripts/CustomJS/admin.js"></script>
 </body>
 </html>
--- spat/Webpages/AdminPortal.aspx.cs
+++ spat/Webpages/AdminPortal.aspx.cs
@@ -15,6 +15,7 @@ namespace spat.Webpages
         protected void Page_Load(object sender, EventArgs e)
         {
             _serverConnection = new ServerConnectionManager();
+            tbUserName.Focus();
         }
 
         protected void btnLogin_Click(object sender, EventArgs e)
@@ -67,14 +68,23 @@ namespace spat.Webpages
 
             foreach (var question in surveyQuestions)
             {
+                adminPortalForm.Controls.Add(new LiteralControl("<div class=\"question\">"));
+                adminPortalForm.Controls.Add(new LiteralControl("<div class=\"row\">"));
+                adminPortalForm.Controls.Add(new LiteralControl("<div class=\"col-lg-12\">"));
                 GenerateAdminPortalHeader(question);
+                adminPortalForm.Controls.Add(new LiteralControl("</div></div>"));
+                adminPortalForm.Controls.Add(new LiteralControl("<div class=\"row\">"));
+                adminPortalForm.Controls.Add(new LiteralControl("<div class=\"col-lg-12\">"));
                 GenerateQuestionEditor(question);
+                adminPortalForm.Controls.Add(new LiteralControl("</div></div>"));
+                adminPortalForm.Controls.Add(new LiteralControl("<div class=\"row\">"));
+                adminPortalForm.Controls.Add(new LiteralControl("<div class=\"col-lg-12\">"));
 
-
-                var answerLabel = new Label { Text = "Answers:" };
-                answerLabel.Font.Bold = true;
+                var answerLabel = new Label { Text = "Answers"};
+                adminPortalForm.Controls.Add(new LiteralControl("<div class=\"lbl-qa\">"));
                 adminPortalForm.Controls.Add(answerLabel);
-                adminPortalForm.Controls.Add(new LiteralControl("<br/>"));
+                adminPortalForm.Controls.Add(new LiteralControl("</div></div></div>"));
+                adminPortalForm.Controls.Add(new LiteralControl("<div class=\"row\">"));
 
                 var answers = _serverConnection.GetSurveyAnswers(question.QuestionId);
 
@@ -83,11 +93,12 @@ namespace spat.Webpages
                     switch (question.QuestionType)
                     {
                         case 0:
-                            var surveyAnswerType0 = new TextBox { Text = answer.AnswerText, TextMode = TextBoxMode.MultiLine, Width = 500, Height = 30 };
-                            var answerDelete = new Button { Text = "Delete", CommandName = "DeleteAnswer()" };
+                            var surveyAnswerType0 = new TextBox { Text = answer.AnswerText, TextMode = TextBoxMode.MultiLine, CssClass = "answer-input" };
+                            var answerDelete = new Button { Text = "Delete", CommandName = "DeleteAnswer()", CssClass = "btn-delete" };
+                            adminPortalForm.Controls.Add(new LiteralControl("<div class=\"col-xs-12 col-sm-6 col-md-4 col-lg-3\">"));
                             adminPortalForm.Controls.Add(surveyAnswerType0);
                             adminPortalForm.Controls.Add(answerDelete);
-                            adminPortalForm.Controls.Add(new LiteralControl("<br/>"));
+                            adminPortalForm.Controls.Add(new LiteralControl("</div>"));
                             break;
                         case 1:
                             //GenerateTextInputQuestion(question);
@@ -108,30 +119,28 @@ namespace spat.Webpages
 
 
                 }
-
-                adminPortalForm.Controls.Add(new LiteralControl("<br/>"));
+                adminPortalForm.Controls.Add(new LiteralControl("</div></div>"));
             }
         }
 
         private void GenerateAdminPortalHeader(QuestionModel question)
         {
-            var questionIdLabel = new Label { Text = string.Format("Question {0}: ", question.QuestionId) };
-            questionIdLabel.Font.Bold = true;
+            var questionIdLabel = new Label { Text = string.Format("Question {0} ", question.QuestionId)};
 
-            var questionLabel = new Label { Text = "Question:" };
-            questionLabel.Font.Bold = true;
+            var questionLabel = new Label { Text = "Question"};
 
+            adminPortalForm.Controls.Add(new LiteralControl("<div class=\"question-header\">"));
             adminPortalForm.Controls.Add(questionIdLabel);
-            adminPortalForm.Controls.Add(new LiteralControl("<br/>"));
+            adminPortalForm.Controls.Add(new LiteralControl("</div>"));
+            adminPortalForm.Controls.Add(new LiteralControl("<div class=\"lbl-qa\">"));
             adminPortalForm.Controls.Add(questionLabel);
-            adminPortalForm.Controls.Add(new LiteralControl("<br/>"));
+            adminPortalForm.Controls.Add(new LiteralControl("</div>"));
         }
 
         private void GenerateQuestionEditor(QuestionModel question)
         {
-            var surveyQuestion = new TextBox { Text = question.QuestionText, TextMode = TextBoxMode.MultiLine, Width=500, Height= 30 };
+            var surveyQuestion = new TextBox { Text = question.QuestionText, TextMode = TextBoxMode.MultiLine, CssClass= "question-input" };
             adminPortalForm.Controls.Add(surveyQuestion);
-            adminPortalForm.Controls.Add(new LiteralControl("<br/>"));
         }
 
         private void DeleteAnswer()
